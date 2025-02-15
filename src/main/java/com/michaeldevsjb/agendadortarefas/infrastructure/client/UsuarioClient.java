65d0a609b1854ab52252410d2054322dbb44c062
MichaelGonçalves <mgdevelopersjb@gmail.com>
2025-02-15 10:50:36 -0300

    correção de apontamentos

--- src/main/java/com/michaeldevsjb/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/michaeldevsjb/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "usuario", url = "${usuario.url}")
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email, @RequestHeader("Authoorization") String token);
-
 
 
 }
