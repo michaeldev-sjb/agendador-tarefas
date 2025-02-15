1346f74cd5039c4ebdeda8d21045f5fdfa1c7c3d
MichaelGonçalves <mgdevelopersjb@gmail.com>
2025-02-15 09:10:50 -0300

    commit inicial

---
+++ src/test/java/com/michaeldevsjb/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaeldevsjb.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
